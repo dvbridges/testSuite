374d16fb05570425ef493d8c0af19fda9650372e
dvbridges <david-bridges@hotmail.co.uk>
2018-12-06 10:06:51 +0000

    _

--- winText.groovy
+++ winText.groovy
@@ -6,9 +6,4 @@ class B{
     def greet(name){ return "greet from B: $name!" }
 }
 
-// this method just to have nice access to create class by name
-Object getProperty(String name){
-    return this.getClass().getClassLoader().loadClass(name).newInstance();
-} 
-
 return this
